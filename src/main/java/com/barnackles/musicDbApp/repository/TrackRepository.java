3c5b2d1fd314cf1e7062a701bcbc0de0f9dbba3a
barnackles <p.martewicz@yahoo.com>
2023-01-19 20:09:42 +0100

    Services

--- src/main/java/com/barnackles/musicDbApp/repository/TrackRepository.java
+++ src/main/java/com/barnackles/musicDbApp/repository/TrackRepository.java
@@ -5,8 +5,12 @@ import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @Transactional
 public interface TrackRepository extends JpaRepository<Track, Long> {
+
+    List<Track> findTracksByEditionId(Long editionId);
 
 }
